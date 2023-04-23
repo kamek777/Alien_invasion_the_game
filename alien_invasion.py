31ea6245220383163563e913cd5e696bb08d351c
Kamil <116445995+kamek777@users.noreply.github.com>
2023-04-23 11:07:01 +0200

    Add files via upload

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,14 +2,17 @@ import sys
 
 import pygame
 
+from settings import Settings
+
 class AlienInvasion:
     """Ogólna klasa przeznaczona do zarządzania zasobami i sposobem działania gry."""
     
     def __init__(self):
         """Inicjalizacja gry i utworzenie jej zasobów."""
         pygame.init()
+        self.settings = Settings()
         
-        self.screen = pygame.display.set_mode((1200,800))
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Inwazja obcych")
         
     def run_game(self):
@@ -19,6 +22,9 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+                    
+            #Odświeżenie ekranu w trakcie każdej iteracji pętli.
+            self.screen.fill(self.settings.bg_color)
             
             #Wyświetlenie ostatnio zmodyfikowanego ekranu.
             pygame.display.flip()
---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    """Klasa przeznaczona do przechowywania wszystkich ustawień gry."""
+    
+    def __init__(self):
+        """Inicjalizacja ustawień gry."""
+        #Ustawienia ekranu.
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (150,200,230)
