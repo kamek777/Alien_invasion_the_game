079de62c92a6fe7a8d9643b62bbfb928b3fd1d81
kamil_7 <kamil.kwiek@student.pk.edu.pl>
2023-05-02 17:51:53 +0200

    Dodanie rzędu obcych.

--- alien_invasion.py
+++ alien_invasion.py
@@ -92,19 +92,27 @@ class AlienInvasion:
         #Utworzenie obcego i ustalenie liczby obcych, którzy zmieszczą się w rzędzie.
         #Odległość między poszczególnymi obcymi jest równa szerokości obcego.
         alien = Alien(self)
-        alien_width = alien.rect.width
-        available_space_x = self.settings.screen_width - (2* alien_width)
-        number_aliens_x = available_space_x // (2* alien_width)
-        #Utworzenie pierwszego rzędu obcych.
-        for alien_number in range(number_aliens_x):
-            self._create_alien(alien_number)
-            
-    def _create_alien(self, alien_number):
+        alien_width, alien_height = alien.rect.size
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        number_aliens_x = available_space_x // (2 * alien_width)
+        
+        #Ustawienie, ile rzędów obcych zmieści się na ekranie.
+        ship_height = self.ship.rect.height
+        available_space_y = (self.settings.screen_height - (3 * alien_height) - ship_height)
+        number_rows = available_space_y // (2 * alien_height)
+        
+        #Utworzenie pełnej floty obcych.
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number, row_number)
+                
+    def _create_alien(self, alien_number, row_number):
         """Utworzenie obcego i umieszczenie go w rzędzie.""" 
         alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
         alien.x = alien_width + 2 * alien_width * alien_number
         alien.rect.x = alien.x
+        alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)   
     
     def _update_screen(self):
