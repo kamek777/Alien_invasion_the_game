f81b3ddf243c5e40bf83670b02c97ca746e54cb1
kamil_7 <kamil.kwiek@student.pk.edu.pl>
2023-05-05 15:10:09 +0200

    Aktualizacja

--- settings.py
+++ settings.py
@@ -6,7 +6,7 @@ class Settings:
         #Ustawienia dotyczące ekranu.
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (230,230,230)
+        self.bg_color = (200,100,250)
         
         #Ustawienia dotyczące statku.
         self.ship_speed = 1.5
