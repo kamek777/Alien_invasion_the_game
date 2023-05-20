e218b8c760ceda33d33b3d3964230f5266c3c85b
kamil_7 <kamil.kwiek@student.pk.edu.pl>
2023-05-21 00:05:55 +0200

    Aktualizacja

--- alien_invasion.py
+++ alien_invasion.py
@@ -292,7 +292,6 @@ class AlienInvasion:
                 self._ship_hit()
                 break
             
-
 if __name__ == '__main__':
     #Utworzenie egzemplarza gry i jej uruchomienie.
     ai = AlienInvasion()
